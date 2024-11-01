d71d6b72fe31da47217fabb453901e5f438f3e1f
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-11-01 18:35:04 -0300

    criacao do update

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -9,9 +9,15 @@ namespace Blog
         {
             using (var context = new DataContext())
             {
-                var tag = new Tag { Name = "Asp.NET", Slug = "aspnet" };
-                context.Tags.Add(tag);
-                context.SaveChanges(); 
+                // var tag = new Tag { Name = "Asp.NET", Slug = "aspnet" };
+                // context.Tags.Add(tag);
+                // context.SaveChanges();
+                var tag = context.Tags.FirstOrDefault(x => x.Id == 2);
+                tag.Name = ".net";
+                tag.Slug = "dotnet";
+
+                context.Update(tag);
+                context.SaveChanges();
             }
         }
     }
