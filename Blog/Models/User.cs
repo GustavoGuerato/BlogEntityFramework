2037a9eb13929f60316d053fd1c6d6c32f703b86
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-11-01 13:42:18 -0300

    criacao do projeto e inicio dos models

---
+++ Blog/Models/User.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models
+{
+    //     [Id] INT NOT NULL IDENTITY(1, 1),
+    // [Name] NVARCHAR(80) NOT NULL,
+    // [Email] VARCHAR(200) NOT NULL,
+    // [PasswordHash] VARCHAR(255) NOT NULL,
+    // [Bio] TEXT NOT NULL,
+    // [Image] VARCHAR(2000) NOT NULL,
+    // [Slug] VARCHAR(80) NOT NULL,
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+
+        public required string Email { get; set; }
+
+        public required string PasswordHash { get; set; }
+
+        public required string Bio { get; set; }
+
+        public required string Image{ get; set; }
+
+        public required string Slug { get; set; }
+    }
+}
