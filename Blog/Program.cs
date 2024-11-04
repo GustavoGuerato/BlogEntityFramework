6484dd8f26feaebbe138e98c29587020dc2cc2eb
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-11-04 19:12:32 -0300

    utilização de include

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -10,31 +10,35 @@ namespace Blog
         {
             using var context = new DataContext();
 
-            var user = new User
-            {
-                Name = "André Baltieri",
-                Slug = "andrebaltieri",
-                Email = "andre@balta.io",
-                Bio = "9x Microsoft MVP",
-                Image = "https://balta.io",
-                PasswordHash = "123098457",
-            };
-            var category = new Category { Name = "Backend", Slug = "Backend" };
+            // var user = new User
+            // {
+            //     Name = "André Baltieri",
+            //     Slug = "andrebaltieri",
+            //     Email = "andre@balta.io",
+            //     Bio = "9x Microsoft MVP",
+            //     Image = "https://balta.io",
+            //     PasswordHash = "123098457",
+            // };
+            // var category = new Category { Name = "Backend", Slug = "Backend" };
 
-            var post = new Post
-            {
-                Author = user,
-                category = category,
-                Body = "<p>Hello world</p>",
-                Slug = "comecando-com-ef-core",
-                Summary = "Neste artigo vamos aprender EF core",
-                Title = "Começando com EF Core",
-                CreateDate = DateTime.Now,
-                LastUpdateDate = DateTime.Now,
-            };
+            // var post = new Post
+            // {
+            //     Author = user,
+            //     category = category,
+            //     Body = "<p>Hello world</p>",
+            //     Slug = "comecando-com-ef-core",
+            //     Summary = "Neste artigo vamos aprender EF core",
+            //     Title = "Começando com EF Core",
+            //     CreateDate = DateTime.Now,
+            //     LastUpdateDate = DateTime.Now,
+            // };
 
-            context.Posts.Add(post);
-            context.SaveChanges();
+            // context.Posts.Add(post);
+            // context.SaveChanges();
+
+            var posts = context.Posts.AsNoTracking().OrderBy(x => x.LastUpdateDate).ToList();
+            foreach (var post in posts)
+                System.Console.WriteLine($"{post.Title} foi escrito por {post.Author?.Name}");
         }
     }
 }
