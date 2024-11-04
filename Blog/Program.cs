0f2a49e9fea444833c3fb0d17ef6df22c763e34e
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-11-04 18:51:57 -0300

    uso de subconjuntos

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -8,7 +8,33 @@ namespace Blog
     {
         static void Main(string[] args)
         {
-            using (var context = new DataContext()) { }
+            using var context = new DataContext();
+
+            var user = new User
+            {
+                Name = "André Baltieri",
+                Slug = "andrebaltieri",
+                Email = "andre@balta.io",
+                Bio = "9x Microsoft MVP",
+                Image = "https://balta.io",
+                PasswordHash = "123098457",
+            };
+            var category = new Category { Name = "Backend", Slug = "Backend" };
+
+            var post = new Post
+            {
+                Author = user,
+                category = category,
+                Body = "<p>Hello world</p>",
+                Slug = "comecando-com-ef-core",
+                Summary = "Neste artigo vamos aprender EF core",
+                Title = "Começando com EF Core",
+                CreateDate = DateTime.Now,
+                LastUpdateDate = DateTime.Now,
+            };
+
+            context.Posts.Add(post);
+            context.SaveChanges();
         }
     }
 }
