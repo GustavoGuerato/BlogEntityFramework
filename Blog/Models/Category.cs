aa78f9e31b58eefe859303a5d1d3b0f497df7233
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-11-04 16:40:24 -0300

    utilizacao de required max/minlenght e column

--- Blog/Models/Category.cs
+++ Blog/Models/Category.cs
@@ -1,14 +1,29 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Blog.Models
 {
+    [Table("Category")]
     public class Category
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(80)]
+        [Column("Name", TypeName = "NVARCHAR")]
         public required string Name { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(80)]
+        [Column("Name", TypeName = "VARCHAR")]
         public required string Slug { get; set; }
     }
 }
--- Blog/Program.cs
+++ Blog/Program.cs
@@ -8,32 +8,7 @@ namespace Blog
     {
         static void Main(string[] args)
         {
-            using (var context = new DataContext())
-            {
-                // var tag = new Tag { Name = "Asp.NET", Slug = "aspnet" };
-                // context.Tags.Add(tag);
-                // context.SaveChanges();
-                // var tag = context.Tags.FirstOrDefault(x => x.Id == 2);
-                // tag.Name = ".net";
-                // tag.Slug = "dotnet";
-
-                // context.Update(tag);
-                // context.SaveChanges();
-
-                // var tag = context.Tags.FirstOrDefault(x => x.Id == 2);
-
-                // context.Remove(tag);
-                // context.SaveChanges();
-
-                // var tags = context.Tags.AsNoTracking().ToList();
-                // foreach (var tag in tags)
-                // {
-                //     System.Console.WriteLine(tag.Name);
-                // }
-
-                var tag = context.Tags.AsNoTracking().SingleOrDefault(t => t.Id == 3);
-                System.Console.WriteLine(tag?.Name);
-            }
+            using (var context = new DataContext()) { }
         }
     }
 }
